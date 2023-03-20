1dcac57927b7466a831598520b1b7d0069a6441f
luchilucero <109253079+luchilucero@users.noreply.github.com>
2023-03-20 11:43:31 -0300

    error login

--- src/main/java/com/portfolio/llf/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/llf/Security/Controller/AuthController.java
@@ -29,7 +29,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
 @CrossOrigin(origins = {"https://frontend-llf.web.app"})
 
 public class AuthController {
@@ -45,7 +44,7 @@ public class AuthController {
     JwtProvider jwtProvider;
     
    
-    @PostMapping("/nuevo")
+    @PostMapping("/auth/nuevo")
     public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
         
         if(bindingResult.hasErrors())
@@ -70,7 +69,7 @@ public class AuthController {
         return new ResponseEntity(new Mensaje("Usuario guardado"), HttpStatus.CREATED);
     }
     
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
         if(bindingResult.hasErrors())
             return new ResponseEntity (new Mensaje("Campos incorrectos"), HttpStatus.BAD_REQUEST);
